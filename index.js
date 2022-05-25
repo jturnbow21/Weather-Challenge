8add418188b7d46a62b1e1ff982b9f625be54a04
jturnbow21 <97416132+jturnbow21@users.noreply.github.com>
2022-05-25 17:42:03 -0500

    added

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+var ZIP_CODE = "74854"
+// function addZipCodeListener () {
+//   ZIP_CODE = document.getElementById("addZip").value;
+//   parseInt(ZIP_CODE);
+//   console.log(ZIP_CODE)
+// }
 const WEATHER_API_KEY = "2f941b77d49f414abfb133019221905";
-const FORECAST_ENDPOINT = `http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=74854`;
+
+
+const FORECAST_ENDPOINT = `http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${ZIP_CODE}&days=5`;
+
 
 window.onload = () => {
   attachGetWeatherDataButtonListener();
 };
-
+function addZipCodeListener () {
+  zipCode = document.getElementById("Zip").value;
+  console.log(zipCode)
+}
 function attachGetWeatherDataButtonListener() {
   const getWeatherDataButton = document.getElementById("get-weather-data");
   if (getWeatherDataButton) {
@@ -15,18 +27,33 @@ function attachGetWeatherDataButtonListener() {
 function addDataToPreTag(data) {
   const resultsPreTag = document.getElementById("data-results");
   const resultsPreTag2 = document.getElementById("temp");
-  const resultsPreTag3 = document.getElementById("condition")
-  resultsPreTag.innerText = data.location.name;
-  resultsPreTag2.innerText = data.current.temp_f;
-  // resultsPreTag3.innerText = data.condition.icon;
+  const currentCondition = document.getElementById("currentCondition")
+  const space = ", ";
+  const fahrenheit = "°F";
+  const currentlyItIs = "Currently it is: "
+  resultsPreTag.innerText = data.location.name + space + data.location.region;
+  resultsPreTag2.innerText = data.current.temp_f + fahrenheit;
+  currentCondition.innerText = currentlyItIs + data.current.condition.text;
+
+  
 }
+
+function addIcon(data) {
+  
+  document.getElementById('condition').src = data.current.condition.icon;
+}
+ 
+
 
 async function getWeatherData() {
   try {
     const res = await fetch(FORECAST_ENDPOINT);
     const data = await res.json();
     addDataToPreTag(data);
+    addIcon(data);
+    // addZipCodeListener
     console.log(data);
+    // console.log(zipCode)
   } catch (error) {
     console.log(error);
   }
